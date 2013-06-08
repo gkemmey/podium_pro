778851a15bbbab1388af80f9d2550ee84fbd5997
wkemmey <whit@1990.usna.com>
2013-06-08 17:52:21 -0400

    creates critique and lesson models, thereby fixing broken migration

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content
   belongs_to :user
+  has_many :critiques
 
   before_save :strip_youtube_id
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130608190233) do
+ActiveRecord::Schema.define(:version => 20130608204141) do
 
   create_table "critiques", :force => true do |t|
     t.integer  "micropost_id"
